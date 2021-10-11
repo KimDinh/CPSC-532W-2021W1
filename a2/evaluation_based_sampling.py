11cc029a21c9971e0efae17caa611e974e3c1da5
KimDinh <hoangkimdinh1608@gmail.com>
2021-10-10 18:54:09 -0700

    A2: implemented primitives and evaluation based sampling; passed initial tests

--- a2/evaluation_based_sampling.py
+++ a2/evaluation_based_sampling.py
@@ -1,6 +1,8 @@
+import numbers
+import torch
 from daphne import daphne
 from tests import is_tol, run_prob_test,load_truth
-
+from primitives import funcprimitives
         
 def evaluate_program(ast):
     """Evaluate a program as desugared by daphne, generate a sample from the prior
@@ -8,7 +10,62 @@ def evaluate_program(ast):
         ast: json FOPPL program
     Returns: sample from the prior of ast
     """
-    return None
+    
+    sigma = {}
+    local_env = {}
+    func_defs = {}
+    
+    for i in range(len(ast)):
+        if isinstance(ast[i], list) and ast[i][0] == "defn":
+            func_defs[ast[i][1]] = {"vars": ast[i][2], "exp": ast[i][3]}
+    
+    ast = ast[-1]
+    ret, sigma = eval(ast, sigma, local_env, func_defs)
+
+    return ret
+
+def eval(e, sigma, local_env, func_defs):
+    if (not isinstance(e, list)) or len(e) == 1:
+        if isinstance(e, list):
+            e = e[0]
+        # case c
+        if isinstance(e, numbers.Number):
+            return torch.tensor(float(e)), sigma
+        # case v
+        else:
+            return local_env[e], sigma
+    # case (let [v1 e1] e0)        
+    elif e[0] == "let":
+        c1, sigma = eval(e[1][1], sigma, local_env, func_defs)
+        return eval(e[2], sigma, {**local_env, **{e[1][0]: c1}}, func_defs)
+    # case (if e1 e2 e3)
+    elif e[0] == "if":
+        e1_, sigma = eval(e[1], sigma, local_env, func_defs)
+        if e1_:
+            return eval(e[2], sigma, local_env, func_defs)
+        else:
+            return eval(e[3], sigma, local_env, func_defs)
+    # case (sample d)
+    elif e[0] == "sample":
+        dist, sigma = eval(e[1], sigma, local_env, func_defs)
+        return dist.sample(), sigma
+    # case (observe d y)
+    elif e[0] == "observe":
+        # behave like sample for now
+        return eval(["sample", e[1]], sigma, local_env, func_defs)
+    # case (e0 e1 ... en)
+    else:
+        c = []
+        for i in range(1,len(e)):
+            ci, sigma = eval(e[i], sigma, local_env, func_defs)
+            c.append(ci)
+        # case e0 is f
+        if e[0] in func_defs:
+            return eval(func_defs[e[0]]["exp"], sigma,
+                        {**local_env, **dict(zip(func_defs[e[0]]["vars"], c))}, func_defs)
+        # case e0 is c
+        elif e[0] in funcprimitives:
+            return funcprimitives[e[0]](*c), sigma
 
 
 def get_stream(ast):
@@ -22,9 +79,9 @@ def run_deterministic_tests():
     
     for i in range(1,14):
         #note: this path should be with respect to the daphne path!
-        ast = daphne(['desugar', '-i', '../CS532-HW2/programs/tests/deterministic/test_{}.daphne'.format(i)])
+        ast = daphne(['desugar', '-i', '../a2/programs/tests/deterministic/test_{}.daphne'.format(i)])
         truth = load_truth('programs/tests/deterministic/test_{}.truth'.format(i))
-        ret, sig = evaluate_program(ast)
+        ret = evaluate_program(ast)
         try:
             assert(is_tol(ret, truth))
         except AssertionError:
@@ -43,7 +100,7 @@ def run_probabilistic_tests():
     
     for i in range(1,7):
         #note: this path should be with respect to the daphne path!        
-        ast = daphne(['desugar', '-i', '../CS532-HW2/programs/tests/probabilistic/test_{}.daphne'.format(i)])
+        ast = daphne(['desugar', '-i', '../a2/programs/tests/probabilistic/test_{}.daphne'.format(i)])
         truth = load_truth('programs/tests/probabilistic/test_{}.truth'.format(i))
         
         stream = get_stream(ast)
@@ -62,8 +119,9 @@ if __name__ == '__main__':
     
     run_probabilistic_tests()
 
-
+    
     for i in range(1,5):
-        ast = daphne(['desugar', '-i', '../CS532-HW2/programs/{}.daphne'.format(i)])
+        ast = daphne(['desugar', '-i', '../a2/programs/{}.daphne'.format(i)])
         print('\n\n\nSample of prior of program {}:'.format(i))
-        print(evaluate_program(ast)[0])
+        print(evaluate_program(ast))
+    
--- a2/primitives.py
+++ a2/primitives.py
@@ -1,3 +1,81 @@
 import torch
+import operator as op
+import copy
+import torch.distributions as dist
 
-#TODO
+def vector(*args):
+    try:
+        return torch.stack(list(args))
+    except:
+        return list(args)
+
+def hashmap(*args):
+    e = list(args)
+    # tensor.item() to get the value in the tensor
+    return dict(zip([e_.item() for e_ in e[0::2]], e[1::2]))
+
+def get(e1, e2):
+    if isinstance(e1, dict):
+        return e1[e2.item()]
+    else:
+        return e1[int(e2)]
+
+def append(e1, e2):
+    ret = copy.deepcopy(e1)
+    if isinstance(ret, torch.Tensor):
+        ret = torch.cat([ret, e2.unsqueeze(dim=0)])
+    else:
+        ret.append(e2)
+    return ret
+
+def put(e1, e2, e3):
+    ret = copy.deepcopy(e1)
+    if isinstance(ret, dict):
+        ret[e2.item()] = e3
+    else:
+        ret[int(e2)] = e3
+    return ret
+
+def cons(e1, e2):
+    ret = copy.deepcopy(e2)
+    if isinstance(ret, torch.Tensor):
+        ret = torch.cat([e1.unsqueeze(dim=0), ret])
+    else:
+        ret = ret.insert(e1, 0)
+    return ret
+
+
+funcprimitives = {
+    '+': op.add,
+    '-': op.sub,
+    '*': op.mul,
+    '/': op.truediv,
+    'sqrt': lambda e: torch.sqrt(e),
+    '>': op.gt,
+    '<': op.lt,
+    '>=':op.ge,
+    '<=':op.le,
+    '=': op.eq,
+    'vector': vector,
+    'hash-map': hashmap,
+    'get': get,
+    'first': lambda e: get(e, 0),
+    'second': lambda e: get(e, 1),
+    'last': lambda e: get(e, -1),
+    'rest': lambda e: e[1:],
+    'append': append,
+    'put': put,
+    'cons': cons,
+    'conj': append,
+    'mat-mul': torch.matmul,
+    'mat-add': op.add,
+    'mat-transpose': lambda e: e.T,
+    'mat-tanh': torch.tanh,
+    'mat-repmat': lambda e1, e2, e3: e1.repeat(int(e2), int(e3)),
+    'normal': dist.Normal,
+    'uniform': dist.Uniform,
+    'beta': dist.Beta,
+    'bernoulli': dist.Bernoulli,
+    'exponential': dist.Exponential,
+    'discrete': dist.Categorical
+}
