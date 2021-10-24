e79242dce8d32ed2358042505d3b15a359449784
KimDinh <hoangkimdinh1608@gmail.com>
2021-10-24 00:23:34 -0700

    convert observed values in graph to tensors; tried to vectorize log_alpha computation

--- a3/mh_gibbs.py
+++ a3/mh_gibbs.py
@@ -13,22 +13,27 @@ class MH_Gibbs:
         self.graph = graph[1]
         self.expr = graph[2]
         self.X = [v for v in self.graph['V'] if v not in self.graph['Y']]
+        self.graph['Y'] = {k: torch.tensor(v) for k, v in self.graph['Y'].items()}
   
     def accept(self, x, X_p, X):
-        q = deterministic_eval(self.graph['P'][x], {**X, **self.graph['Y']})
-        q_p = deterministic_eval(self.graph['P'][x], {**X_p, **self.graph['Y']})
+        q = deterministic_eval(self.graph['P'][x][1], {**X, **self.graph['Y']})
+        q_p = deterministic_eval(self.graph['P'][x][1], {**X_p, **self.graph['Y']})
         log_alpha = q_p.log_prob(X[x]) - q.log_prob(X_p[x])
-        for v in self.graph['A'][x]:
-            vars = {**X_p, **self.graph['Y']}
-            log_alpha += deterministic_eval(self.graph['P'][v], vars).log_prob(vars[v])
-            vars = {**X, **self.graph['Y']}
-            log_alpha -= deterministic_eval(self.graph['P'][v], vars).log_prob(vars[v])
 
+        Vx = [x, *self.graph['A'][x]]
+        vars = {**X_p, **self.graph['Y']}
+        log_prob_Vx = [deterministic_eval(self.graph['P'][v][1], vars).log_prob(vars[v]) for v in Vx]
+        log_alpha += torch.sum(torch.stack(log_prob_Vx))
+
+        vars = {**X, **self.graph['Y']}
+        log_prob_Vx = [deterministic_eval(self.graph['P'][v][1], vars).log_prob(vars[v]) for v in Vx]
+        log_alpha -= torch.sum(torch.stack(log_prob_Vx))
+        
         return torch.exp(log_alpha)
 
     def gibbs_step(self, X):
         for x in self.X:
-            q = deterministic_eval(self.graph['P'][x], {**X, **self.graph['Y']})
+            q = deterministic_eval(self.graph['P'][x][1], {**X, **self.graph['Y']})
             X_p = copy.deepcopy(X)
             X_p[x] = q.sample()
             alpha = self.accept(x, X_p, X)
@@ -40,8 +45,9 @@ class MH_Gibbs:
     def gibbs(self, X_init, num_samples):
         samples = []
         for i in range(num_samples):
-            samples.append(self.gibbs_step(X_init if i == 0 else samples[i-1]))
-        
+            X = self.gibbs_step(X_init if i == 0 else samples[i-1])
+            samples.append(deterministic_eval(self.expr, {**X, **self.graph['Y']}))
+
         return samples
     
     def sample(self, num_samples):
