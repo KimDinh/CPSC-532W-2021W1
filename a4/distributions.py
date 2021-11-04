2e595327cba02ea0ac530dbf930498cd06a50a15
KimDinh <hoangkimdinh1608@gmail.com>
2021-11-03 22:03:15 -0700

    fix bug in making copies of distribution

--- a4/distributions.py
+++ a4/distributions.py
@@ -3,13 +3,14 @@ import torch.distributions as dist
 
 class Normal(dist.Normal):
     
-    def __init__(self, loc, scale):
-        
-        if scale > 20.:
-            self.optim_scale = scale.clone().detach().requires_grad_()
+    def __init__(self, loc, scale, copy=False):
+        if not copy:
+            if scale > 20.:
+                self.optim_scale = scale.clone().detach().requires_grad_()
+            else:
+                self.optim_scale = torch.log(torch.exp(scale) - 1).clone().detach().requires_grad_()
         else:
-            self.optim_scale = torch.log(torch.exp(scale) - 1).clone().detach().requires_grad_()
-        
+            self.optim_scale = scale
         
         super().__init__(loc, torch.nn.functional.softplus(self.optim_scale))
     
@@ -24,7 +25,7 @@ class Normal(dist.Normal):
         
         ps = [p.clone().detach().requires_grad_() for p in self.Parameters()]
          
-        return Normal(*ps)
+        return Normal(*ps, copy=True)
     
     def log_prob(self, x):
         
@@ -113,12 +114,14 @@ class Dirichlet(dist.Dirichlet):
 
 class Gamma(dist.Gamma):
     
-    def __init__(self, concentration, rate):
-        if rate > 20.:
-            self.optim_rate = rate.clone().detach().requires_grad_()
+    def __init__(self, concentration, rate, copy=False):
+        if not copy:
+            if rate > 20.:
+                self.optim_rate = rate.clone().detach().requires_grad_()
+            else:
+                self.optim_rate = torch.log(torch.exp(rate) - 1).clone().detach().requires_grad_()
         else:
-            self.optim_rate = torch.log(torch.exp(rate) - 1).clone().detach().requires_grad_()
-        
+            self.optim_rate = rate
         
         super().__init__(concentration, torch.nn.functional.softplus(self.optim_rate))
     
@@ -133,7 +136,7 @@ class Gamma(dist.Gamma):
         
         concentration,rate = [p.clone().detach().requires_grad_() for p in self.Parameters()]
         
-        return Gamma(concentration, rate)
+        return Gamma(concentration, rate, copy=True)
 
     def log_prob(self, x):
         
